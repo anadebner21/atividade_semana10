cbf0a42d3f5137711d82083897290a3211ad92a2
Ana Paula <anapdebner21@gmail.com>
2021-10-27 14:14:08 -0300

    Modal

--- script-navbar.js
+++ script-navbar.js
@@ -1,6 +1,18 @@
 
-		<script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
-		crossorigin="anonymous"
-	  ></script>
+<script
+rc="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+crossorigin="anonymous"
+></script>
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  
+  var button = event.relatedTarget
+  
+  var recipient = button.getAttribute('data-bs-whatever')
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'Nova mensagem para: ' + recipient
+  modalBodyInput.value = recipient
+})
